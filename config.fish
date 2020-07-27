744c93ecd019a275ab5a74300911a6b302934a8a
masa <masatoshi.sboard@gmail.com>
2020-07-27 14:59:35 +0900

    changed settings

--- config.fish
+++ config.fish
@@ -1,15 +1,31 @@
+# setting for xdg config dir
+set XDG_CONFIG_DIRS /etc/xdg
+
+# settings for peco key bindings
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
   bind \cx\ck peco_kill
   bind \c] peco_select_ghq_repository
   bind \cx\cr peco_recentd
 end
-set -U FZF_LEGACY_KEYBINDINGS 0
+
+# setting for pyenv
 set PATH $HOME/.pyenv/shims $PATH
 eval (pyenv init - | source)
+
+# settings for node
 set -x PATH $HOME/.nodebrew/current/bin $PATH
-set XDG_CONFIG_DIRS /etc/xdg
+
+# setting for rbenv
 rbenv init - | source
+
+# path for flutter
 set -x PATH /Users/masatoshi/git/github.com/flutter/flutter/bin $PATH
+
+# paths for go
 set -x GOPATH $HOME/dev
 set -x PATH $PATH $GOPATH/bin
+
+# setting for fzf
+set -U FZF_LEGACY_KEYBINDINGS 0
+set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
