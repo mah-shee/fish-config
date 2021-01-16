13447259d57115de0f7fabbb73e09bef3ae66118
masa <masatoshi.sboard@gmail.com>
2021-01-16 12:10:01 +0900

    add paths

--- config.fish
+++ config.fish
@@ -33,3 +33,22 @@ set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
 set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
 set -gx LDFLAGS "-L/usr/local/opt/llvm/lib"
 set -gx CPPFLAGS "-I/usr/local/opt/llvm/include"
+# ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+test -f /Users/masatoshi/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /Users/masatoshi/.ghcup/bin $PATH
+
+# For compilers to find readline you may need to set:
+set -gx LDFLAGS "-L/usr/local/opt/readline/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/readline/include"
+
+# For pkg-config to find readline you may need to set:
+set -gx PKG_CONFIG_PATH "/usr/local/opt/readline/lib/pkgconfig"
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+
+set -gx LDFLAGS "-L/usr/local/opt/sqlite/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/sqlite/include"
+
+#For pkg-config to find sqlite you may need to set:
+set -gx PKG_CONFIG_PATH "/usr/local/opt/sqlite/lib/pkgconfig"
+set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
